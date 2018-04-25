7afdbe96f6f3202bd1fad0bc0532d8758c2273bf
lutsiukAV <nastyalutsuk1995@gmail.com>
2018-04-25 23:06:37 +0300

    angular controllers added

---
+++ pages/js/app.js
@@ -0,0 +1,95 @@
+var app = angular.module("SocModule", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "index.htm"
+    })
+    .when("/signin", {
+        templateUrl : "signin.htm"
+    })
+    .when("/posts", {
+        templateUrl : "posts.htm"
+    })
+});
+
+app.controller("UserController", function ($scope, $http) {
+    $scope.users = [];
+    $scope.data = {
+        username: "",
+        email: "",
+        password: ""
+    };
+    
+    $scope.sign_up = function () {
+        var method = "POST";
+        var url = "http://localhost:8000/users/";
+
+        var params = JSON.stringify($scope.data);
+
+        $http({
+            method: method,
+            url: url,
+            data: params,
+            headers: {'Content-Type':'application/json'}
+        }).then(_success, _error)
+        
+    };
+
+    $scope.sign_in = function () {
+        var method = "POST";
+        var url = "http://localhost:8000/auth/";
+
+        var params = JSON.stringify({username: $scope.data.username, password: $scope.data.password});
+
+        $http({
+            method: method,
+            url: url,
+            data: params,
+            headers: {'Content-Type':'application/json'}
+        }).then(_success, _error)
+    };
+
+    $scope.logout = function () {
+        document.cookie = "token=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+        window.location.replace("http://localhost:8000/signin")
+    };
+
+    function _success(response) {
+        token = response.data.token;
+        document.cookie = "token=" + token;
+        window.location.replace("http://localhost:8000/posts")
+    }
+
+    function _error(repsonse) {
+        console.log(response.statusText)
+
+    }
+});
+
+
+app.controller("PostsController", function ($scope, $http) {
+    $scope.posts = [];
+    $scope.data = {
+        title: "",
+        description: ""
+    };
+
+    _refreshPosts();
+
+
+
+    function _refreshPosts(){
+        $http({
+            method: "GET",
+            url: "http://localhost:8000/posts/"
+        }).then(function successCallback(response) {
+            $scope.data = response.data;
+
+        }, function errorCallback(response) {
+            console.log(response.statusText)
+
+        })
+    }
+
+});
